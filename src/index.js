7cb35bc0f3c9c31ff24b0306b2d8e569546d1d5f
apoorvsrivastava270 <metalruleskjs@gmail.com>
2022-09-04 19:04:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+// Get the input field
+var input = document.getElementById("searchbar");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("search").click();
+  }
+}); 
+
+
 function logochange() {
     var x = document.getElementById("searchbar").value;
     var L = x.split(" ");
